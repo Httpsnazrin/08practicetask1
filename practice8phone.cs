5de94d464b6eb4907ebffed589c1ef2ccc461d47
Httpsnazrin <147836567+Httpsnazrin@users.noreply.github.com>
2024-02-15 14:14:35 +0400

    Add files via upload

---
+++ practice8phone.cs
@@ -0,0 +1,7 @@
+﻿using practice08task1;
+
+Phone phone = new Phone(1, "Galaxy S21", "Samsung", 1500, "Android", 108);
+
+Console.WriteLine($"Price with tax: { phone.CalcPriceWithTax()}");
+phone.DisplayInfo();
+phone.CheckCamera();
